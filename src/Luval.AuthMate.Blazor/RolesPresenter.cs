f3660f7bd6d8b0de9a5c1fea3bca01dbb74d1a10
Oscar Marin <oscar@marin.cr>
2024-12-03 21:22:40 -0600

    Create RolesPresenter.cs

---
+++ src/Luval.AuthMate.Blazor/RolesPresenter.cs
@@ -0,0 +1,42 @@
+﻿using Luval.AuthMate.Entities;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Luval.AuthMate.Blazor
+{
+    public class RolesPresenter
+    {
+        IAuthMateContext _context;
+
+        public RolesPresenter(IAuthMateContext context)
+        {
+            _context = context;
+        }
+
+        public void LoadData()
+        {
+            Data = _context.Roles;
+        }
+
+        public void ApplyNameFilter(string filter)
+        {
+
+            if (string.IsNullOrEmpty(filter))
+            {
+                LoadData();
+                return;
+            }
+
+
+            Data = _context.Roles.Where(i => !string.IsNullOrEmpty(i.Name) && i.Name.Contains(filter));
+        }
+
+
+        public IQueryable<Role> Data { get; private set; }
+    }
+}
