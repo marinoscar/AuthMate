9564d4854abd479287511e41610703222f9109ac
Oscar Marin <oscar@marin.cr>
2024-12-31 11:50:15 -0600

    Update Program.cs
    basic configuration

--- src/Luval.AuthMate.Sample/Program.cs
+++ src/Luval.AuthMate.Sample/Program.cs
@@ -7,12 +7,26 @@ namespace Luval.AuthMate.Sample
     {
         public static void Main(string[] args)
         {
+            //TODO: Add config settings for the the Client ID and ClientSecret in the appsettings.json file and secrets.json
+
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
             builder.Services.AddRazorComponents()
                 .AddInteractiveServerComponents();
             builder.Services.AddFluentUIComponents();
+
+            //Add logging services
+            builder.Services.AddLogging(logging =>
+            {
+                logging.AddConsole();
+                logging.AddDebug();
+            });
+
+            //Add the controllers and the http client and context accessor
+            builder.Services.AddControllers();
+            builder.Services.AddHttpClient();
+            builder.Services.AddHttpContextAccessor();
 
             var app = builder.Build();
 
@@ -25,6 +39,13 @@ namespace Luval.AuthMate.Sample
             }
 
             app.UseHttpsRedirection();
+
+            //Map the controllers and the razor components
+            app.MapControllers();
+            app.UseRouting();
+            //Add the authentication and authorization
+            app.UseAuthorization();
+            app.UseAuthentication();
 
             app.UseStaticFiles();
             app.UseAntiforgery();
