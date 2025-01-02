a9eb526af9d2f8fd464eef45027aa1b037e6087f
Oscar Marin <oscar@marin.cr>
2025-01-01 19:30:09 -0600

    Create AuthController.cs
    added a generic controller

---
+++ src/Luval.AuthMate/Web/Controllers/AuthController.cs
@@ -0,0 +1,38 @@
+﻿using Luval.AuthMate.Core.Entities;
+using Luval.AuthMate.Core.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Luval.AuthMate.Web.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly AppConnectionService _appConnection;
+
+        public AuthController(AppConnectionService appConnectionService)
+        {
+            _appConnection = appConnectionService ?? throw new ArgumentNullException(nameof(appConnectionService));
+        }
+
+        [HttpGet("login/{provider}")]
+        public IActionResult Login(string provider)
+        {
+            var config = _providers.FirstOrDefault(p => p.Name.Equals(provider, StringComparison.OrdinalIgnoreCase));
+            if (config == null) return BadRequest("Provider not supported.");
+
+            var loginUrl = $"{config.AuthorizationEndpoint}?response_type=code" +
+                           $"&client_id={config.ClientId}" +
+                           $"&redirect_uri={config.RedirectUri}" +
+                           $"&scope={Uri.EscapeDataString(config.Scopes)}";
+
+            return Redirect(loginUrl);
+        }
+
+    }
+}
