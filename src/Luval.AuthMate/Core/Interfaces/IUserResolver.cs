63c3bc2e125e612f30256a0bcddbf8f2ecf62dce
Oscar Marin <oscar@marin.cr>
2025-01-03 16:31:35 -0600

    Update IUserResolver.cs
    updated interface

--- src/Luval.AuthMate/Core/Interfaces/IUserResolver.cs
+++ src/Luval.AuthMate/Core/Interfaces/IUserResolver.cs
@@ -26,5 +26,24 @@ namespace Luval.AuthMate.Core.Interfaces
         /// <returns>The current web user as an <see cref="AppUser"/> object.</returns>
         /// <exception cref="InvalidOperationException">Thrown when the HttpContext is null.</exception>
         AppUser GetUser();
+
+        /// <summary>
+        /// Gets the timezone of the current web user.
+        /// </summary>
+        /// <returns>
+        /// The timezone of the current web user as a <see cref="TimeZoneInfo"/> object.
+        /// If the user is not authenticated or the timezone is not set, returns "Central Standard Time".
+        /// </returns>
+        /// <exception cref="InvalidOperationException">Thrown when the HttpContext is null.</exception>
+        TimeZoneInfo GetUserTimezone();
+
+        /// <summary>
+        /// Converts the specified UTC date and time to the current web user's local date and time.
+        /// </summary>
+        /// <param name="dateTime">The UTC date and time to convert.</param>
+        /// <returns>The converted date and time in the user's local timezone.</returns>
+        /// <exception cref="InvalidOperationException">Thrown when the HttpContext is null.</exception>
+        DateTime ConvertToUserDateTime(DateTime dateTime);
+        
     }
 }
