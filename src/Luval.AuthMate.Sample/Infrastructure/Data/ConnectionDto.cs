931fed5b4dc6f8796c68224c5dd962a3c5efdbf4
Oscar Marin <oscar@marin.cr>
2025-01-03 12:35:21 -0600

    Create ConnectionDto.cs

---
+++ src/Luval.AuthMate.Sample/Infrastructure/Data/ConnectionDto.cs
@@ -0,0 +1,12 @@
+﻿using Luval.AuthMate.Infrastructure.Configuration;
+
+namespace Luval.AuthMate.Sample.Infrastructure.Data
+{
+    public class ConnectionDto
+    {
+        public ulong? Id { get; set; }
+        public string? ProviderName { get; set; }
+        public string? Scopes { get; set; }
+
+    }
+}
